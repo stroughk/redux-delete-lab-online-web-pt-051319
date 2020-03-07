637f04fa46322aa2e47590efdfcdc7a0612b91d6
Kateryna Strough <katerynastrough@hotmail.com>
2020-03-07 21:24:49 +0000

    Automatically backed up by Learn

--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import BandInput from './BandInput';
 import Bands from './Bands';
 import { connect } from 'react-redux'
-
 class BandsContainer extends Component {
   render() {
     return (
@@ -13,12 +12,9 @@ class BandsContainer extends Component {
     )
   }
 }
-
 const mapStateToProps = ({ bands }) => ({ bands })
-
 const mapDispatchToProps = dispatch => ({
   addBand: name => dispatch({ type: "ADD_BAND", name }),
   deleteBand: id => dispatch({type: "DELETE_BAND", id})
 })
-
 export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer)
